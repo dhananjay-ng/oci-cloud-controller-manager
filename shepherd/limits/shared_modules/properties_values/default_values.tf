bf875e55b95870405524020378621bcc217978e3
Goutham M L <goutham.m.l@oracle.com>
2025-09-09 14:14:43 +0530

    update image validator pop

--- shepherd/limits/shared_modules/properties_values/default_values.tf
+++ shepherd/limits/shared_modules/properties_values/default_values.tf
@@ -1,6 +1,6 @@
 locals {
   // Update the pop version corresponding to the pop build for app release
-  pop_version = "a15758ce7b7_120"
+  pop_version = "9e7e5f160b1_122"
 
   // Update the ccm image sha value here for updating CCM versions for respective k8s versions across all realms
   ccm_default_mapping = {
