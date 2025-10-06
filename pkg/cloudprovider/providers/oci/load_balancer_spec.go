f705ff74e3f08e21eb7f1f262606d1f208ecbc12
sumantac <sumanta.c.chatterjee@oracle.com>
2025-10-06 17:04:44 +0530

    Listener update

--- pkg/cloudprovider/providers/oci/load_balancer_spec.go
+++ pkg/cloudprovider/providers/oci/load_balancer_spec.go
@@ -481,7 +481,7 @@ func NewLBSpec(logger *zap.SugaredLogger, svc *v1.Service, provisionedNodes []*v
 	}
 
 	listeners, err := getListeners(svc, sslConfig, convertOciIpVersionsToOciIpFamilies(versions.ListenerBackendIpVersion))
-	logger.Info("Listener Specs created %v", listeners)
+	logger.Infof("Listener Specs created +%v with sslConfig +%v", listeners, sslConfig)
 	if err != nil {
 		return nil, err
 	}
@@ -1477,7 +1477,7 @@ func getListenersOciLoadBalancer(svc *v1.Service, sslCfg *SSLConfig) (map[string
 		var secretName string
 		var err error
 		var sslConfiguration *client.GenericSslConfigurationDetails
-		if sslCfg != nil && len(sslCfg.ListenerSSLSecretName) != 0 {
+		if sslCfg != nil && (len(sslCfg.ListenerSSLSecretName) != 0 || len(sslCfg.ListenerPostSSLMap) != 0) {
 			secretName = sslCfg.ListenerSSLSecretName
 			listenerCipherSuiteAnnotation, _ := svc.Annotations[ServiceAnnotationLoadbalancerListenerSSLConfig]
 			sslConfiguration, err = getSSLConfiguration(sslCfg, secretName, port, listenerCipherSuiteAnnotation)
