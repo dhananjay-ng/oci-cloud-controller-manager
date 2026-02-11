914e1c4fa7d5331b589ce65e60eb07763affa767
Goutham M L <goutham.m.l@oracle.com>
2026-02-11 19:45:33 +0530

    fix pop version

--- shepherd/limits/shared_modules/properties_values/default_values.tf
+++ shepherd/limits/shared_modules/properties_values/default_values.tf
@@ -1,6 +1,6 @@
 locals {
   // Update the pop version corresponding to the pop build for app release
-  pop_version = "d0cf7f0b528_179"
+  pop_version = "bcbced4b9f2_190"
 
   // Update the ccm image sha value here for updating CCM versions for respective k8s versions across all realms
   ccm_default_mapping = {
